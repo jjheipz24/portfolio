256ff121f2a9ca194652d09ef3111887713c4519
Jin Jin Heipler <jheipler@linksquares.com>
2023-07-17 01:37:31 -0400

    added initial hero banner

---
+++ src/components/avatar.tsx
@@ -0,0 +1,9 @@
+export const Avatar = () => {
+  return (
+    <img
+      src='https://models.readyplayer.me/64b44e33194df77f5cc1a0d0.png?blendShapes[Wolf3D_Head][mouthSmile]=0.4'
+      alt='avatar'
+      className='h-full'
+    />
+  );
+};
