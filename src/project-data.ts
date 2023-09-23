306c271f721bcdc3420366b09f80c5dbd7a33326
Jin Jin Heipler <jheipler@linksquares.com>
2023-09-23 18:06:05 -0400

    added project data file and basic cards with project routing

---
+++ src/project-data.ts
@@ -0,0 +1,34 @@
+const projects = [
+  {
+    title: 'Sprout',
+    body: 'This is a description',
+    slug: 'sprout',
+    chips: ['Web Application', 'React', 'Node.js'],
+    tech: [
+      'HTML',
+      'SCSS',
+      'React',
+      'Pixi.js',
+      'Node.js',
+      'Express',
+      'MongoDB',
+      'Figma',
+      'Adobe Illustrator',
+      'Adobe After Effects',
+    ],
+  },
+  {
+    title: 'Grammasaurus-Rex',
+    body: 'This is a description',
+    slug: 'g-rex',
+    chips: ['Web Application', 'Vue.js', 'Grammarbot'],
+  },
+  {
+    title: '2018 U.S. Hate Crimes',
+    body: 'This is a description',
+    slug: 'hate-crimes',
+    chips: ['Data Visualization', 'D3.js', 'OpenRefine'],
+  },
+];
+
+export default projects;
