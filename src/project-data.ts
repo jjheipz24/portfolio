45fa0942f3a5bb86d703f7ac9783ca5c04558440
Jin Jin Heipler <heipler.jj@gmail.com>
2025-10-27 21:30:13 -0400

    added personal images

--- src/project-data.ts
+++ src/project-data.ts
@@ -1,33 +1,28 @@
 const projects = [
   {
     title: 'Sprout',
-    body: 'This is a description',
+    body: 'An application where users can grow virtual gardens by sending and receiving positive messages.',
     slug: 'sprout',
     chips: ['Web Application', 'React', 'Node.js'],
-    tech: [
-      'HTML',
-      'SCSS',
-      'React',
-      'Pixi.js',
-      'Node.js',
-      'Express',
-      'MongoDB',
-      'Figma',
-      'Adobe Illustrator',
-      'Adobe After Effects',
-    ],
+    tech: ['HTML', 'SCSS', 'React', 'Pixi.js', 'Node.js', 'MongoDB'],
+    summary: [],
+    images: [],
   },
   {
     title: 'Grammasaurus-Rex',
-    body: 'This is a description',
+    body: 'A digital tool to help students practice grammar skills.',
     slug: 'g-rex',
     chips: ['Web Application', 'Vue.js', 'Grammarbot'],
+    summary: [],
+    images: [],
   },
   {
     title: '2018 U.S. Hate Crimes',
-    body: 'This is a description',
+    body: 'An interactive data visualization of hate crimes in the United States in 2018.',
     slug: 'hate-crimes',
     chips: ['Data Visualization', 'D3.js', 'OpenRefine'],
+    summary: [],
+    images: [],
   },
 ];
 
