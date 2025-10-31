c9f2040957edf7a3c775f6f72828d0d0051f3989
Jin Jin Heipler <heipler.jj@gmail.com>
2025-10-30 22:11:08 -0400

    attempt at improving styling

--- src/components/avatar.tsx
+++ src/components/avatar.tsx
@@ -3,7 +3,7 @@ export const Avatar = () => {
     <img
       src='https://models.readyplayer.me/64b44e33194df77f5cc1a0d0.png?blendShapes[Wolf3D_Head][mouthSmile]=0.4'
       alt='avatar'
-      className='max-h-[50vh] md:max-h-[60vh] object-contain'
+      className='max-h-[50vh] md:max-h-[60vh] object-contain mb-[-8px] sm:mb-[-12px]'
     />
   );
 };
