0fda39c971b74f3ba1aa0e59c82653c0edb4dd32
Jin Jin Heipler <heipler.jj@gmail.com>
2025-10-30 21:56:41 -0400

    made img be background

--- src/components/avatar.tsx
+++ src/components/avatar.tsx
@@ -3,7 +3,7 @@ export const Avatar = () => {
     <img
       src='https://models.readyplayer.me/64b44e33194df77f5cc1a0d0.png?blendShapes[Wolf3D_Head][mouthSmile]=0.4'
       alt='avatar'
-      className='h-full'
+      className='max-h-[50vh] md:max-h-[60vh] object-contain'
     />
   );
 };
